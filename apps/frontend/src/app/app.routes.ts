c3acef6f773b019573c419650d7c2a1ef4d54ab0
Oana Goldberg <goldbergoanna@gmail.com>
2025-11-09 17:41:04 +0200

    feat(frontend): routes and guard added

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthGuard } from '@core/auth/guard/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'pokedex',
+    canActivate: [() => inject(AuthGuard).canActivate()],
+    loadComponent: () =>
+      import('@features/pokedex/pokedex.component').then(
+        (m) => m.PokedexComponent,
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'pokedex',
+  },
+  {
+    path: '**',
+    redirectTo: 'pokedex',
+  },
+];
